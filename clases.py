488da496dfc61d191f4f5eb658f9f14dd3079773
ALUMNO <caxajgabriel2@gmail.com>
2025-08-13 19:49:33 -0600

    agregar usuario

--- clases.py
+++ clases.py
@@ -47,15 +47,50 @@ class ListaLibros:
                 print("El libro no existe")
 
 class Usuario:
-    def __init__(self, nombre, carrera):
+    def __init__(self, carne, nombre, carrera):
+        self.carne = carne
         self.nombre = nombre
         self.carrera = carrera
 
     def get_usuario(self):
-        return f"Nombre: {self.nombre} - Carrera: {self.carrera}"
+        return f"Carne: {self.carne} - Nombre: {self.nombre} - Carrera: {self.carrera}"
 
 class ListaUsuarios:
     def __init__(self):
         self.usuarios = {}
 
+    def agregar(self):
+        carne = input("Ingrese el carne del usuario: ")
+
+        if carne in self.usuarios:
+            print("El libro ya existe")
+        else:
+            self.usuarios[carne] = Usuario(
+                input("Ingrese el carne del usuario: "),
+                input("Ingrese el Nombre: "),
+                input("Ingrese la carrera: ")
+            )
+            print("...Usuario agregado\n")
+
+    def mostrar(self):
+        if not self.usuarios:
+            print("No hay usuarios")
+        else:
+            print("#LISTA DE LIBROS")
+            for codigo, datos in self.libros:
+                print(f"Codigo: {codigo} - {datos.get_libro()}\n")
+                print("...")
+
+    def eliminar(self):
+        if not self.libros:
+            print("No hay libros")
+        else:
+            codigo = input("Ingrese el codigo del libro: ")
+            if codigo in self.libros:
+                del self.libros[codigo]
+                print("...Libro eliminado\n")
+            else:
+                print("El libro no existe")
+
 class Gestiones:
+    pass
