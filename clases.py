3aa4d01c4c85b9730f07b5262605b5090e9b9314
ALUMNO <caxajgabriel2@gmail.com>
2025-08-13 19:40:35 -0600

    funcion para mostra los libros y eliminar

--- clases.py
+++ clases.py
@@ -20,10 +20,31 @@ class ListaLibros:
         else:
             self.libros[codigo] = Libro(
                 input("Ingrese el codigo del libro: "),
-                input("Ingrese el titulo del libro: "),
-                input("Ingrese el autor del libro: "),
-                input("Ingrese el anio del libro: ")
+                input("Ingrese el titulo: "),
+                input("Ingrese el autor: "),
+                input("Ingrese el anio: ")
             )
+            print("...Libro agregado\n")
+
+    def mostrar(self):
+        if not self.libros:
+            print("No hay libros")
+        else:
+            print("#LISTA DE LIBROS")
+            for codigo, datos in self.libros:
+                print(f"Codigo: {codigo} - {datos.get_libro()}\n")
+                print("...")
+
+    def eliminar(self):
+        if not self.libros:
+            print("No hay libros")
+        else:
+            codigo = input("Ingrese el codigo del libro: ")
+            if codigo in self.libros:
+                del self.libros[codigo]
+                print("...Libro eliminado\n")
+            else:
+                print("El libro no existe")
 
 class Usuario:
     def __init__(self, nombre, carrera):
